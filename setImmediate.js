1d2b2490822bebd9dbff465436db0fc53f5da7da
idjem <mejdiourir@gmail.com>
2017-02-23 10:44:52 +0100

    global => _global

--- setImmediate.js
+++ setImmediate.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const global = (Function('return this'))();
+const _global = (Function('return this'))();
 
 /*istanbul ignore next*/
-module.exports = global.setImmediate || global.setTimeout;
+module.exports = _global.setImmediate || _global.setTimeout;
